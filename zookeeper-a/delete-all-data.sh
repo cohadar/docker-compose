76d29004bee787315c2aa388f1aab1d7f2cb6b08
cohadar <cohadar@gmail.com>
2019-03-09 15:28:19 +0100

    data

--- zookeeper-a/delete-all-data.sh
+++ zookeeper-a/delete-all-data.sh
@@ -2,3 +2,5 @@
 rm -rf data-a1/zookeeper/version-2/*
 rm -rf data-a2/zookeeper/version-2/*
 rm -rf data-a3/zookeeper/version-2/*
+rm -rf data-a4/zookeeper/version-2/*
+rm -rf data-a5/zookeeper/version-2/*
